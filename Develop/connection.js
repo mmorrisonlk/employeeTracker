6262614e7a05ab42b0e4bde3727d14645cc663cd
mmorrisonlk <mmorrisonlk@gmail.com>
2021-06-30 22:42:43 -0500

    Merging two works

--- Develop/connection.js
+++ Develop/connection.js
@@ -2,8 +2,9 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: 'localhost',
+    port: '3006',
     user: 'root',
-    password: 'AttackOnRoot',
+    password: 'Attackonroot',
     database: 'employees'
 });
 
--- Develop/schema.sql
+++ Develop/schema.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS employees;
+
 CREATE DATABASE employees;
 
 USE employees;
